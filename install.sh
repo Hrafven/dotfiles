aa2ec472247484e6cfafae9e7506e730afdee72c
Hrafven <hrafven@gmail.com>
2022-07-22 15:18:19 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 sudo pacman -Sy --needed --noconfirm \
 	curl \
 	emacs \
 	git \
 	wget \
+	ripgrep \
 	zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ -d "$ZSH" ]; then
+	echo "warning: oh-my-zsh already exists -- updating"
+	"$ZSH"/tools/upgrade.sh
+else
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+	echo "warning: powerlevel10k already exists -- updating"
+	git -C "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k pull
+else
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+fi
 
-cp -f $SCRIPT_DIR/.p10k.zsh $HOME
-cp -f $SCRIPT_DIR/.zshrc $HOME
-cp -f $SCRIPT_DIR/.alacritty.yml $HOME
-cp -f $SCRIPT_DIR/aliases.zsh $HOME/.oh-my-zsh/custom/
-cp -f -R $SCRIPT_DIR/.doom.d $HOME
+ln -s -f "$SCRIPT_DIR"/.p10k.zsh -t "$HOME"
+ln -s -f "$SCRIPT_DIR"/.zshrc -t "$HOME"
+ln -s -f "$SCRIPT_DIR"/.alacritty.yml -t "$HOME"
+ln -s -f "$SCRIPT_DIR"/aliases.zsh -t "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+if [ -d "$SCRIPT_DIR"/.doom.d ]; then
+	rm -rf "$SCRIPT_DIR"/.doom.d
+	mkdir "$SCRIPT_DIR"/.doom.d
+fi
+ln -s -f "$SCRIPT_DIR"/.doom.d/*.el -t "$HOME"
 
-source $HOME/.zshrc
+echo "source $HOME/.zshrc to apply configuration"
 
