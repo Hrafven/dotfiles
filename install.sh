0d835948e4bbf613b1e9403d9b839429118d3010
Hrafven <hrafven@gmail.com>
2022-07-22 14:04:47 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -9,10 +9,14 @@ sudo pacman -Sy --needed --noconfirm \
 	wget \
 	zsh
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp -f $SCRIPT_DIR/.p10k.zsh
+cp -f $SCRIPT_DIR/.zshrc .
 cp -f $SCRIPT_DIR/alacritty.yml $HOME
 cp -f $SCRIPT_DIR/aliases.sh $HOME/.oh-my-zsh/custom/
 cp -f -R $SCRIPT_DIR/.doom.d $HOME
-cp -f $SCRIPT_DIR/.zshrc .
 
 source $HOME/.zshrc
 
