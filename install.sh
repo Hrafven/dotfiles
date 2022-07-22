93d4168fbf08e696a8e24e3fca71b994056257f9
Hrafven <hrafven@gmail.com>
2022-07-22 15:45:31 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ if [ -d "$ZSH" ]; then
 else
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
 if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
 	echo "warning: powerlevel10k already exists -- updating"
 	git -C "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k pull
@@ -23,15 +24,23 @@ else
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 fi
 
+if [ -d "$HOME"/.emacs.d ]; then
+	echo "warning: doomemacs already exists -- updating"
+	"$HOME"/.emacs.d/bin/doom upgrade -!
+else
+	git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME"/.emacs.d
+	"$HOME"/.emacs.d/bin/doom install -!
+fi
+
 ln -s -f "$SCRIPT_DIR"/.p10k.zsh -t "$HOME"
 ln -s -f "$SCRIPT_DIR"/.zshrc -t "$HOME"
 ln -s -f "$SCRIPT_DIR"/.alacritty.yml -t "$HOME"
 ln -s -f "$SCRIPT_DIR"/aliases.zsh -t "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
-if [ -d "$SCRIPT_DIR"/.doom.d ]; then
-	rm -rf "$SCRIPT_DIR"/.doom.d
-	mkdir "$SCRIPT_DIR"/.doom.d
+if [ -d "$HOME"/.doom.d ]; then
+	rm -rf "$HOME"/.doom.d
 fi
-ln -s -f "$SCRIPT_DIR"/.doom.d/*.el -t "$HOME"
+mkdir "$HOME"/.doom.d
+ln -s -f "$SCRIPT_DIR"/.doom.d/*.el -t "$HOME"/.doom.d
 
 echo "source $HOME/.zshrc to apply configuration"
 
