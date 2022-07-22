2945a7516409fc9d4ae237f35fe778f82869ea76
Hrafven <hrafven@gmail.com>
2022-07-22 14:23:39 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ sudo pacman -Sy --needed --noconfirm \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp -f $SCRIPT_DIR/.p10k.zsh
-cp -f $SCRIPT_DIR/.zshrc .
-cp -f $SCRIPT_DIR/alacritty.yml $HOME
-cp -f $SCRIPT_DIR/aliases.sh $HOME/.oh-my-zsh/custom/
+cp -f $SCRIPT_DIR/.p10k.zsh $HOME
+cp -f $SCRIPT_DIR/.zshrc $HOME
+cp -f $SCRIPT_DIR/.alacritty.yml $HOME
+cp -f $SCRIPT_DIR/aliases.zsh $HOME/.oh-my-zsh/custom/
 cp -f -R $SCRIPT_DIR/.doom.d $HOME
 
 source $HOME/.zshrc
