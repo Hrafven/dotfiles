2584c16cbf7aec8179fc58c5b682d6305fadcd0b
Hrafven <hrafven@gmail.com>
2022-07-29 15:01:38 +0200

    Add aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,10 @@
-alias c=cd
-alias v=$EDITOR
+alias c="cd "
+alias v="$EDITOR "
 alias sudo="sudo "
-alias cl=clear
+alias cl="clear "
 alias rc="$EDITOR $HOME/.zshrc"
 alias rcr="source $HOME/.zshrc"
+alias ale="$EDITOR $HOME/.oh-my-zsh/custom/aliases.zsh"
+
+alias google-chrome="/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe --profile-directory=\"Profile 1\""
+
