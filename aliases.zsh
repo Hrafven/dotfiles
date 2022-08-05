130fc51aa079ace64312de806d92af7242542512
Hrafven <hrafven@gmail.com>
2022-08-06 00:48:21 +0200

    Remove sudo alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,7 @@
 alias c="cd "
 alias v="$EDITOR "
-alias sudo="sudo "
+#alias sudo="sudo "
+#alias sudo="nocorrect sudo "
 alias cl="clear "
 alias rc="$EDITOR $HOME/.zshrc"
 alias rcr="source $HOME/.zshrc"
