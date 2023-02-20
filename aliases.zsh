7d9d5b741c5964e9960f4b4337672bdd38ca1912
Hrafven <hrafven@gmail.com>
2023-02-20 11:31:42 +0100

    Remove everything

--- aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias c="cd "
-alias v="$EDITOR "
-#alias sudo="sudo "
-#alias sudo="nocorrect sudo "
-alias cl="clear "
-alias rc="$EDITOR $HOME/.zshrc"
-alias rcr="source $HOME/.zshrc"
-alias ale="$EDITOR $HOME/.oh-my-zsh/custom/aliases.zsh"
-
